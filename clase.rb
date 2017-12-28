b98c1463f777facdebda4bf9d8cef89849105cd3
Juan Carlos <juanc.com@gmail.com>
2017-12-28 07:07:41 -0400

    first commit

---
+++ clase.rb
@@ -0,0 +1,21 @@
+# Clase
+ARGV.each do|a|
+  puts "Argument: #{a}"
+end
+
+require 'date'
+class Clase
+  def initialize(nombre)
+    @nombre = nombre
+  end
+  def saluda
+    puts "Hola #{@nombre}"
+  end
+  def hora
+    puts DateTime.now
+  end
+end
+
+e = Clase.new('Juan')
+e.saluda
+e.hora
